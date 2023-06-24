1436093af017ce42f2bcac07e59dd05d360dfccf
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-24 16:11:39 +0300

    Lesson 10

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,9 @@
+import React from "react";
+
 const Header = () => {
   return (
     <div>
-      <a href="#s">Home</a>
-      <a href="#s">News Feed</a>
-      <a href="#s">Message</a>
+      <a href="#s">Home</a>-<a href="#s">News Feed</a>-<a href="#s">Message</a>
     </div>
   );
 };
---
+++ src/mobydik.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mobydik = () => {
+  return <div>it-kamasutra.com footer</div>;
+};
+
+export default Mobydik;
