41b8cae016d56c663da1ba08f235b78efc6df5b5
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-07-05 21:32:51 +0300

    Lesson 16 - Props_object

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import s from "./Post.module.css";
 
-export const Post = () => {
+type PropsPostType = {
+  message: string;
+  likes: number;
+};
+
+export const Post = (props: PropsPostType) => {
   return (
     <div className={s.item}>
       <img
         src="https://avavatar.ru/images/full/30/pYJBXtXlNkcCZLDa.jpg"
         alt=" venom "
       />
-      post 1
-      <div><span>like</span></div>
+      {props.message}
+      <div>
+        <span>{props.likes} likes</span>
+      </div>
     </div>
   );
 };
