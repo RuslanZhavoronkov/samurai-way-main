42060cf39a9bc9d8d6565cbaa06e0ec6dffc3e0c
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-07-22 22:53:59 +0300

    Lesson 21 - Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,8 +7,21 @@ type PropsType={
 
 export const Dialogs:React.FC<PropsType> = () => {
     return(
-        <div>
-            Dialogs
+        // Две колонки значит две дивки
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>Dimych</div>
+                <div className={s.dialog}>Andrew</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Sasha</div>
+                <div className={s.dialog}>Victor</div>
+                <div className={s.dialog}>Valera</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
