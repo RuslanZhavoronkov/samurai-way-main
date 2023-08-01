67fb26a34bef5cecf30bebb63417fb29457a5d64
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-08-01 12:48:50 +0300

    Lesson_offline_Dimych - Deep Copy

--- src/lesson46.js
+++ src/lesson46.js
@@ -25,3 +25,66 @@ console.log(a);
 
 console.log(a === b);
 console.log(a.students === b.students);
+
+//------------------------------------------------------------------------------------------------------------
+
+const state = {
+    name: 'it-kamasutra.com',
+    age: 1,
+    school: {
+        address: 'Minsk',
+    }
+}
+
+//поменяй возраст на два
+
+const b = {...state, age: 2}
+
+const chill = {...state, school: {...state.school}}
+
+console.log (state.age);
+
+//-------------------------------------------------------------------------------------
+
+const firstSchool = {
+    name: 'it-kamasutra.com',
+    age: 1,
+    school: {
+        address: 'Minsk',
+        director: {
+            name: 'Andrey'
+        }
+    }
+}
+
+const secondSchool = {... firstSchool, school:{...firstSchool.school, director: {...firstSchool.school.director}}}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+let firstArray = [{name: 'A'}, {name: 'B'}]
+
+let secondArray = firstArray.map(el => ({...el}))
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+let threeArray = [{name: 'A', a:{}}, {name: 'B', a:{}}]
+
+let fourArray = threeArray.map(el => ({...el, a: {...el.a}}))
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+
+const threeSchool = {
+    name: 'it-kamasutra.com',
+    age: 1,
+    school: {
+        students:[{}, {}, {}],
+        address: 'Minsk',
+        director: {
+            name: 'Andrey'
+        }
+    }
+}
+
+
+const fourSchool = {...threeSchool, school: {...threeSchool.school, students:threeSchool.school.students.map(el => ({...el})), director: {...threeSchool.school.director}}}
