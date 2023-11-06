a0a248d9aacbd1c67131c074fd289a10b6b1b60c
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-11-06 12:17:02 +0300

    lesson 93 - Paginator

--- src/components/Users/Paginator/Paginator.tsx
+++ src/components/Users/Paginator/Paginator.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { PaginationType, UsersServerType } from "../../../redux/usersReducer";
 import s from "./Paginator.module.css";
 
@@ -5,38 +6,81 @@ type PaginationPropsType = {
   users: UsersServerType;
   pagination: PaginationType;
   onPageChanged: (pageNumber: number) => void;
+  portionSize?: number; //размер порции
 };
 
-export const Paginator: React.FC<PaginationPropsType> = (
-  {
-    users,
-    pagination,
-    onPageChanged
-  }
-) => {
+export const Paginator: React.FC<PaginationPropsType> = ({
+  users,
+  pagination,
+  onPageChanged,
+  portionSize = 10,
+}) => {
   let pageArray = [];
-  let pagesCount = Math.ceil(
-    users.totalCount / pagination.pageSize
-  );
+  let pagesCount = Math.ceil(users.totalCount / pagination.pageSize);
   for (let i = 1; i <= pagesCount; i++) {
     pageArray.push(i);
   }
+  let portionCount = Math.ceil(pagesCount / portionSize); //кол-во порций (кол-во страниц/размер порции(кол-во страниц в порции) = кол-во порции которое получится)
+  let [portionNumber, setPortionNumber] = useState(1); //локальный state номера порции
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1; //левая граница порции
+  let rightPortionPageNumber = portionNumber * portionSize; //правая граница порции
+  const decreasePortionNumber = () => {
+    setPortionNumber(portionNumber - 1);
+  };
+  const increasePortionNumber = () => {
+    setPortionNumber(portionNumber + 1)
+  };
+
   return (
-    <div>
-      {pageArray.map((el) => {
-        const boldSpan =
-          pagination.currentPage === el ? s.selectedPage : "";
+    <div className={s.paginator}>
+      {portionNumber > 1 && (
+        <button onClick={decreasePortionNumber}>PREV</button>
+      )}
 
-        const onClickSpanHandler = () => {
-          onPageChanged(el);
-        };
+      {pageArray
+        .filter(
+          (pageNumber) =>
+            pageNumber >= leftPortionPageNumber &&
+            pageNumber <= rightPortionPageNumber
+        )
+        .map((el) => {
+          const boldSpan = pagination.currentPage === el ? `${s.selectedPage} ${s.pageNumber}` : s.pageNumber;
 
-        return (
-          <span className={boldSpan} onClick={onClickSpanHandler}>
-            {el}
-          </span>
-        );
-      })}
+          const onClickSpanHandler = () => {
+            onPageChanged(el);
+          };
+
+          return (
+            <span className={boldSpan} onClick={onClickSpanHandler}>
+              {el}
+            </span>
+          );
+        })}
+      {portionCount > portionNumber && (
+        <button onClick={increasePortionNumber}>NEXT</button>
+      )}
     </div>
   );
 };
+
+//__________________________________________________________________________________________________________________________
+
+// return (
+//   <div className={s.paginator}>
+//     {portionNumber > 1 && <button onClick={increasePortionNumber} />}
+
+//     {pageArray.map((el) => {
+//       const boldSpan = pagination.currentPage === el ? s.selectedPage : "";
+
+//       const onClickSpanHandler = () => {
+//         onPageChanged(el);
+//       };
+
+//       return (
+//         <span className={boldSpan} onClick={onClickSpanHandler}>
+//           {el}
+//         </span>
+//       );
+//     })}
+//   </div>
+// );
